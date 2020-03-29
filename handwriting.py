bec2ae6e66eea08358b5693bf72e6e53ecfd740a
dennybasillie <denny077@hotmail.com>
2020-03-29 16:17:56 +0800

    improve translation quality

--- handwriting.py
+++ handwriting.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import pinyin
 from PyQt5 import uic
@@ -9,61 +10,88 @@ from providers.googleIME import GoogleIMERecognizer
 from providers.googleTranslate import GoogleTranslate
  
 class HandWriting(QMainWindow):
+    LANGUAGES = ["en", "ja", "ko", "zh_CN", "zh_TW"]
     LANGUAGE_WITH_PINYIN = ["zh_CN", "zh_TW"]
 
     def __init__(self):
         super().__init__()
-        uic.loadUi("handwriting.ui", self)
+
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, "handwriting.ui")
+        uic.loadUi(filename, self)
         
         self.setupActions()
         self.setupState()
 
     def setupActions(self):
         brushSizeActions = [self.action_3px, self.action_5px, self.action_7px, self.action_9px]
-        languageActions = [self.action_en, self.action_ja, self.action_ko, self.action_zh_cn, self.action_zh_TW]
-
         brushSizeActionGroup = QActionGroup(self)
         brushSizeActionGroup.setExclusive(True) #TODO: display default value as checked
         for action in brushSizeActions:
             brushSizeActionGroup.addAction(action)
             action.triggered.connect(lambda ind, val = action.text()[:-2]: self.onBrushSizeChanged(val))
 
-        languageActionGroup = QActionGroup(self)
-        languageActionGroup.setExclusive(True) #TODO: display default value as checked
-        for action in languageActions:
-            languageActionGroup.addAction(action)
-            action.triggered.connect(lambda ind, val = action.text(): self.onLanguageChanged(val))
-
+        self.action_fullscreen.triggered.connect(self.toggleFullScreen)
         self.action_tablet.triggered.connect(self.toggleTabletMode)
         self.action_clear.triggered.connect(self.clearCanvas)
         self.action_reset.triggered.connect(self.reset)
         self.action_exit.triggered.connect(self.exit)
 
+        self.rawComboBox.addItems(self.LANGUAGES)
+        self.translatedComboBox.addItems(self.LANGUAGES)
+        self.rawComboBox.setCurrentIndex(self.LANGUAGES.index("zh_CN"))
+        self.translatedComboBox.setCurrentIndex(self.LANGUAGES.index("en"))
+        self.rawComboBox.currentIndexChanged.connect(self.onLanguageChanged)
+        self.translatedComboBox.currentIndexChanged.connect(lambda index: self.onLanguageChanged(index, True))
+
+        if self.rawComboBox.currentText() in self.LANGUAGE_WITH_PINYIN:
+            self.rawPinyin.setVisible(True)
+        else:
+            self.rawPinyin.setVisible(False)
+
     def setupState(self):
         self.canvasWidget.setupState()
-        self.language = "en"
+        sourceLang = self.rawComboBox.currentText()
         self.isTabletMode = False
-        self.recognizer = GoogleIMERecognizer({ 'width': self.canvasWidget.width(), 'height': self.canvasWidget.height(), 'language': self.language })
-        self.translator = GoogleTranslate({ 'source': self.language, 'target': "en" })
+        self.recognizer = GoogleIMERecognizer({ 'width': self.canvasWidget.width(), 'height': self.canvasWidget.height(), 'language':sourceLang })
+        self.translator = GoogleTranslate({ 'source': sourceLang, 'target': self.translatedComboBox.currentText() })
 
     def resizeEvent(self, event):
         self.recognizer.change_area({ 'width': self.canvasWidget.width(), 'height': self.canvasWidget.height() })
 
-    def onLanguageChanged(self, lang):
-        if lang in self.LANGUAGE_WITH_PINYIN:
-            self.rawPinyin.setVisible(True)
+    def onLanguageChanged(self, index, isTarget=False):
+        sourceLang = self.rawComboBox.currentText()
+        targetLang = self.translatedComboBox.currentText()
+        self.translator.change_language({ 'source': sourceLang, 'target':  targetLang })
+        if isTarget:
+            self.refreshTranslated()
         else:
-            self.rawPinyin.setVisible(False)
-        self.language = lang
-        self.recognizer.change_language(lang)
-        self.translator.change_language({ 'source': lang, 'target': "en" })
-        self.reset()
+            if sourceLang in self.LANGUAGE_WITH_PINYIN:
+                self.rawPinyin.setVisible(True)
+            else:
+                self.rawPinyin.setVisible(False)
+            self.recognizer.change_language(sourceLang)
+            self.reset()
+
+    def refreshTranslated(self):
+        rawSentence = self.rawText.toPlainText()
+        if rawSentence:
+            hasPinyin = self.translatedComboBox.currentText() in self.LANGUAGE_WITH_PINYIN
+            candidates = self.translator.translate(rawSentence)
+            self.translatedText.clear()
+            for candidate in candidates:
+                text = f"** {candidate}"
+                if hasPinyin:
+                    translatedPinyin = pinyin.get(candidate, delimiter=" ")
+                    text += f" ({translatedPinyin})"
+                self.translatedText.appendPlainText(text)
 
     def onBrushSizeChanged(self, size):
         self.canvasWidget.setBrushSize(size)
 
     def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Return: 
+        key = event.key()
+        if key == Qt.Key_Return: 
             traces = self.canvasWidget.getTraces()
             if len(traces) > 0:
                 self.recognizer.add_stroke(traces, False)
@@ -73,11 +101,17 @@ class HandWriting(QMainWindow):
                 self.rawText.insertPlainText(result[0])
                 self.canvasWidget.clear()
 
-                rawSentence = self.rawText.toPlainText()
-                translated = self.translator.translate(rawSentence)
-                self.translatedText.setPlainText(translated)
-                if self.rawPinyin.isVisible():
-                    self.rawPinyin.setPlainText(pinyin.get(rawSentence, delimiter=" "))
+            if self.rawPinyin.isVisible():
+                self.rawPinyin.setPlainText(pinyin.get(self.rawText.toPlainText(), delimiter=" "))
+            self.refreshTranslated()
+        elif key == Qt.Key_Escape:
+            self.setFocus()
+
+    def toggleFullScreen(self):
+        if not self.isFullScreen():
+            self.showFullScreen()
+        else: 
+            self.showNormal()
 
     def toggleTabletMode(self):
         self.isTabletMode = self.action_tablet.isChecked()
--- providers/googleTranslate.py
+++ providers/googleTranslate.py
@@ -2,8 +2,8 @@ import requests
 import json
 
 class GoogleTranslate():
-
-    URL = 'https://translate.googleapis.com/translate_a/single?client=gtx&dt=t'
+    #dt=t :: Top; dt=bd :: Alternatives; dt=rm :: Pinyin;
+    URL = 'https://translate.googleapis.com/translate_a/single?client=gtx&dt=t&dt=bd'
     
     def __init__(self, config):
         self.languages = { 'source': config['source'], 'target': config['target'] }
@@ -12,8 +12,16 @@ class GoogleTranslate():
         encoded_query = requests.utils.quote(query)
         full_url = self.URL + f"&sl={self.languages['source']}&tl={self.languages['target']}&q={encoded_query}"
         response = requests.get(full_url)
-        #TODO: check response.status_code and process response.text[0]
-        candidates = json.loads(response.text)[0][0][0]
+        json_response = json.loads(response.text)
+
+        candidates = []
+        if json_response[1] is not None:
+            candidates = json_response[1][0][1]
+        
+        top_candidate = json_response[0][0][0]
+        if top_candidate not in candidates:
+            candidates.insert(0,top_candidate)
+        
         return candidates
 
     def change_language(self, language):
